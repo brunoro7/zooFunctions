bf7c2c136ba89e77b739f48ff127e5e59e3e74f2
brunoro7 <bruno.ro7c@gmail.com>
2022-01-27 18:59:36 -0300

    entregando finalmente o requisito1.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,11 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const listaEspecies = (data.species);
-  listaEspecies.name = animal;
-
-  console.log(listaEspecies);
+  species.forEach((specie) => {
+    
+    console.log(specie.residents);
+  });
 }
-getAnimalsOlderThan();
+getAnimalsOlderThan('penguins', 9);
 
 module.exports = getAnimalsOlderThan;
+
+// const listaEspecies = (data.species);
+
+// for (let i = 0; i < listaEspecies.length; i += 1) {
+//   const nomeAnimal = listaEspecies[i];
+//   const animaisResidentes = Object.values(nomeAnimal.residents);
+
+//   if (animal === nomeAnimal.name) {
+//     console.log(animaisResidentes);
+//     return Object.entries(nomeAnimal);
+//   }
+// }
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,20 +1,20 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const listaEspecies = (data.species);
-  for (let i = 0; i < listaEspecies.length; i += 1) {
-    const idCadaEspecie = listaEspecies[i].id;
-    const cadaEspecieId = listaEspecies[i];
-    if (ids.length <= 0) {
-      return [];
-    }
-    if (ids[0] === idCadaEspecie) {
-      return [cadaEspecieId];
-    }
+  const result = [];
+  if (ids.length <= 0) {
+    return result;
   }
+  if (ids.length === 1) {
+    return [species.find((specie) => specie.id === ids[0])];
+  }
+  ids.forEach((id) => {
+    result.push(species.find((specie) => specie.id === id));
+  });
+  return result;
 }
-getSpeciesByIds();
-getSpeciesByIds('78460a91-f4da-4dea-a469-86fd2b8ccc84', '0938aa23-f153-4937-9f88-4858b24d6bce');
+console.log(getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274', '78460a91-f4da-4dea-a469-86fd2b8ccc84'));
 
 module.exports = getSpeciesByIds;
 // '89be95b3-47e4-4c5b-b687-1fabf2afa274'
