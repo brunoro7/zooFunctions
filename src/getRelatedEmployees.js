c5c8f11b911def5ebbb84eef3c32b72204dde7b5
brunoro7 <bruno.ro7c@gmail.com>
2022-01-28 00:02:24 -0300

    entregando requisito 4, lembrando de como 'refletir na comparação' como o Gean ajudou no exercicio 1.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const estaEmAlgumManager = employees.some((index) => index.managers.includes(id));
+  return (estaEmAlgumManager);
 }
-
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const funcionarioId = employees.find((funcionario) => funcionario.id === managerId);
+  const listaFuncionariosManager = employees
+    .filter((index) => index.managers.includes(funcionarioId.id));
+  return listaFuncionariosManager.map((funcionarioManager) =>
+    `${funcionarioManager.firstName} ${funcionarioManager.lastName}`);
 }
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = { isManager, getRelatedEmployees };
+
+// 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'
+// '9e7d4524-363c-416a-8759-8aa7e50c0992'
