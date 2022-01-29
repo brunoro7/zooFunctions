32e43a10fe12c773f0d8bd7ad5db805d04f02cb8
brunoro7 <bruno.ro7c@gmail.com>
2022-01-29 00:13:34 -0300

    entregando requisito 9, com a ajuda da BRENNDHA-TURMA-XP-TRIBO-B.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalFuncionario = employees.find((empregado) => empregado.id === id).responsibleFor[0];
+  const firstAnimalFuncionario = species.find((firstAnimal) =>
+    firstAnimal.id === animalFuncionario);
+  const saidaDoVelho = firstAnimalFuncionario.residents.reduce((acc, current) =>
+    (current.age > acc.age ? current : acc));
+  return Object.values(saidaDoVelho);
 }
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
