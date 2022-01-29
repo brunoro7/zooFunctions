a17e89d0b05a5fe12a5bd438918d1df8cfa816cb
brunoro7 <bruno.ro7c@gmail.com>
2022-01-29 01:55:05 -0300

    entregando requisito 10, com ajuda do LEO-VOGEL-TURMA-XP-TRIBO-A e BRENNDHA-TURMA-XP-TRIBO-B.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,28 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function filtraObjeto(info) {
+  const employee = employees.find(({ id, firstName, lastName }) =>
+    id === info.id || firstName === info.name || lastName === info.name);
+  if (employee) {
+    const responsAnimais = species.filter((animalRep) =>
+      employee.responsibleFor.includes(animalRep.id));
+    const nomeAnimalFiltrado = responsAnimais.map((animalFiltrado) => animalFiltrado.name);
+    const localAnimalFiltrado = responsAnimais.map((localAnimal) => localAnimal.location);
+    const objBase = {
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: nomeAnimalFiltrado,
+      locations: localAnimalFiltrado,
+    };
+    return objBase;
+  }
+  throw new Error('Informações inválidas');
 }
-
+function getEmployeesCoverage(info) {
+  if (info) {
+    return filtraObjeto(info);
+  }
+  return employees.map((employee) => filtraObjeto({ name: employee.lastName }));
+}
 module.exports = getEmployeesCoverage;
