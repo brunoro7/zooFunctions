0680b267a371f30f4abd4e86389bd99dbec83a87
brunoro7 <bruno.ro7c@gmail.com>
2022-01-28 22:31:45 -0300

    entregando requisito 5, com ajuda da BRENNDHA-TURMA-XP-TRIBO-B.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const ChaveFormada = species.reduce((accumulator, current) => {
+      accumulator[current.name] = current.residents.length;
+      return accumulator;
+    }, {});
+    return ChaveFormada;
+  }
+  const cadaEspecie = species.find((specie) => specie.name === animal.specie);
+
+  const cadaEspecieGenre = cadaEspecie.residents.filter((specieGenre) =>
+    specieGenre.sex === animal.sex);
+
+  return (animal.specie && animal.sex) ? cadaEspecieGenre.length : cadaEspecie.residents.length;
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,5 +1,4 @@
 const { employees } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
 
 function isManager(id) {
   const estaEmAlgumManager = employees.some((index) => index.managers.includes(id));
